f2fe13ca3b66a6d985ca752c5681a99e1d4d2443
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-03 20:26:22 +0530

    BUILDING START

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// Model for course - file
+type Course struct {
+	CourseId string `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string  `json:"website"`
+}
+
+//fake DB
+var courses []Course
+
+// middleware
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
